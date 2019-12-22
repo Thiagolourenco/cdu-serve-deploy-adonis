c839b5d30da7e67f9ceca2cc148978e0855270e2
Thiagolourenco <thiagolourencosaraiva123@gmail.com>
2019-12-22 16:54:13 -0300

    t

--- config/database.js
+++ config/database.js
@@ -18,7 +18,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get("DB_CONNECTION", "sqlite"),
+  connection: Env.get("DATABASE_URL", "pg"),
 
   /*
   |--------------------------------------------------------------------------
@@ -77,11 +77,11 @@ module.exports = {
   pg: {
     client: "pg",
     connection: {
-      host: Env.get("DB_HOST", "local"),
-      port: Env.get("DB_PORT", ""),
-      user: Env.get("DB_USER", "root"),
-      password: Env.get("DB_PASSWORD", ""),
-      database: Env.get("DB_DATABASE", "adonis")
+      host: Env.get("DB_HOST", DATABASE_URL.host),
+      port: Env.get("DB_PORT", DATABASE_URL.port),
+      user: Env.get("DB_USER", DATABASE_URL.username),
+      password: Env.get("DB_PASSWORD", DATABASE_URL.password),
+      database: Env.get("DB_DATABASE", DATABASE_URL.pathname.substr(1))
     },
     debug: Env.get("DB_DEBUG", false)
   }
